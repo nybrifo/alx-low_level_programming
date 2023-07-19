4a19e6f5aada7c0a3b5fa67d9fb1a4cc89bca3b0
nybrifo <nanayawbrifo@gmil.com>
2023-07-18 17:33:30 -0700

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	ptintf("\n");
+	return (0);
+}
