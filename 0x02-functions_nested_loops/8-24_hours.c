a8b0fe559f45ae2dd8cdd1da6fc36564ae80db3d
nybrifo <nanayawbrifo@gmil.com>
2023-07-19 17:49:58 -0700

    jb

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
+
 /**
- * jack_bauer - prints every minute of the day of jack bauer
- *
- * starting from 00:00 to 23:59
- *
+ * jack_bauer - Prints every minute of the day of jack bauer
+ * Return: ...
  */
+
 void jack_bauer(void)
+
 {
 	int i, j;
 
