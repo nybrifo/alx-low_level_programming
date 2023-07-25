09389c1b2a900be7e06c6bb46c683e442b50bda2
nybrifo <nanayawbrifo@gmil.com>
2023-07-24 19:02:46 -0700

    the pointer to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy  copies the string pointed too by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+char *_strrcpy(char *dest, char *src)
+{
+	int z = -1;
+
+	do {
+		z++;
+		dest[z] = src[z];
+	} while (src[z] != '\0');
+
+	return (dest);
+}
