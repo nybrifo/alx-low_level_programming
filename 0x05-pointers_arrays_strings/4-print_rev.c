e1b978ca5199bcccd2c46a962af59125280bee25
nybrifo <nanayawbrifo@gmil.com>
2023-07-24 17:02:42 -0700

    prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 /**
  * print_rev - imprime en reversa
  * @s: string
- * Return: 0
+ * return: 0
  */
 void print_rev(char *s)
 {
 	int longi = 0;
 	int 0;
 
-	while (*s !='\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
